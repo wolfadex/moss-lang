3b33f3e9effb923c0c54fa104e3c1a9a67c16e3c
Wolfgang Schuster <wolfadex@gmail.com>
2025-12-02 14:22:17 -0600

    Remove unused code

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,13 @@
 module Main exposing (..)
 
--- import CodeMirror
-
 import Browser
 import Canonical
-import Editor exposing (Editor)
-import EditorModel
-import EditorMsg
 import Eval
 import Html exposing (Html)
 import Html.Attributes
 import Html.Events
 import Json.Decode
-import Located exposing (Located(..))
 import Source
-import TextEditor
 
 
 main : Program () Model Msg
@@ -30,36 +23,15 @@ main =
 type alias Model =
     { code : String
     , evalModel : Eval.Model
-
-    -- , editor : Editor
-    -- , textEditor : TextEditor.Model
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    -- let
-    --     ( editor, editorCmd ) =
-    --         Editor.init
-    --             { width = windowWidth 1200
-    --             , height = windowHeight 700
-    --             , fontSize = 16
-    --             , verticalScrollOffset = 3
-    --             , viewMode = EditorModel.Light
-    --             , debugOn = True
-    --             , viewLineNumbersOn = True
-    --             , wrapOption = EditorMsg.DontWrap
-    --             }
-    -- in
     ( { code = ""
       , evalModel = Eval.init
-
-      -- , editor = editor
-      -- , textEditor = TextEditor.initWith """Hello
-      -- World!"""
       }
     , Cmd.none
-      -- Cmd.map EditorMessage editorCmd
     )
 
 
@@ -71,8 +43,6 @@ subscriptions _ =
 type Msg
     = CodeChanged String
     | UserWantsToRunCode
-      -- | EditorMessage EditorMsg.EMsg
-      -- | TextEditorMessage TextEditor.Msg
     | EvalMessage Eval.Msg
 
 
@@ -113,34 +83,12 @@ update msg model =
             )
 
 
-
--- EditorMessage emsg ->
---     Editor.update emsg model.editor
---         |> Tuple.mapBoth
---             (\editor -> { model | editor = editor })
---             (Cmd.map EditorMessage)
--- TextEditorMessage tmsg ->
---     TextEditor.update
---         { toModel = \textEditor -> { model | textEditor = textEditor }
---         , toMsg = TextEditorMessage
---         }
---         tmsg
---         model.textEditor
-
-
 view : Model -> Browser.Document Msg
 view model =
     { title = "Moss"
     , body =
         [ Html.text "Moss on a 🪨"
         , Html.br [] []
-
-        -- , Editor.view model.editor
-        --     |> Html.map EditorMessage
-        -- , TextEditor.view
-        --     { toMsg = TextEditorMessage
-        --     }
-        --     model.textEditor
         , Html.textarea
             [ Html.Events.onInput CodeChanged
             , Html.Attributes.value model.code
@@ -200,202 +148,3 @@ decodeTextKeydown =
                         _ ->
                             Json.Decode.fail "ignore"
             )
-
-
-windowWidth : Float -> Float
-windowWidth appWidth =
-    min (0.5 * appWidth) 900
-
-
-windowHeight : Float -> Float
-windowHeight appHeight =
-    appHeight - 70
-
-
-
--- sourceErrorToDiagnostic : Source.DeadEnd -> CodeMirror.Diagnostic
--- sourceErrorToDiagnostic deadEnd =
---     -- { row = Int
---     -- , col = Int
---     -- , problem = problem
---     -- , contextStack = List { row = Int, col = Int, context = context }
---     -- }
---     { from = CodeMirror.LineCol { line = deadEnd.row, col = deadEnd.col }
---     , to = Nothing
---     , severity = CodeMirror.Error
---     , message = Just (Debug.toString deadEnd.problem)
---     }
--- canonicalErrorToDiagnostic : Canonical.Error -> CodeMirror.Diagnostic
--- canonicalErrorToDiagnostic error =
---     case error of
---         Canonical.EmptyFile ->
---             { from = CodeMirror.LineCol { line = 1, col = 1 }
---             , to = Nothing
---             , severity = CodeMirror.Warning
---             , message = Just "No code to run"
---             }
---         Canonical.ExpectedGive (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Nothing -- Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Expected give:"
---             }
---         Canonical.ExpectedGiveQuote (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Expected give: [word]"
---             }
---         Canonical.CantGive (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Can't be given ..."
---             }
---         Canonical.CantUse (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Can't use ..."
---             }
---         Canonical.ExpectedUseQuote (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Expected use: [file://path]"
---             }
---         Canonical.InvalidAlias span alias_ ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just ("Invalid alias: " ++ alias_)
---             }
---         Canonical.InvalidUseUri (Located schemeSpan scheme) (Located pathSpan path) ->
---             { from = CodeMirror.LineCol { line = schemeSpan.start.row, col = schemeSpan.start.column }
---             , to = Just <| CodeMirror.LineCol { line = pathSpan.end.row, col = pathSpan.end.column }
---             , severity = CodeMirror.Error
---             , message = Just (scheme ++ "://" ++ path ++ " can't be used here")
---             }
---         Canonical.InvalidUseFilePath span path ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just ("Invalid file path " ++ path)
---             }
---         Canonical.InvalidFileName span name ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just ("Invalid file name " ++ name)
---             }
---         Canonical.InvalidDefName (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Invalid definition name"
---             }
---         Canonical.ShouldntBeIndented span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Shouldn't be indented"
---             }
---         Canonical.ShouldBeIndented span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Should be indented"
---             }
---         Canonical.InvalidKey span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Invalid key"
---             }
---         Canonical.UnexpectedNamed span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Unexpected name"
---             }
---         Canonical.UnexpectedComment span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Unexpected comment"
---             }
---         Canonical.UnexpectedDocComment span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Unexpected doc comment"
---             }
---         Canonical.UnexpectedTypeDef span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Unexpected type def"
---             }
---         Canonical.UnexpectedWord (Located span word) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Unexpected word ..."
---             }
---         Canonical.UnsupportedUri (Located span uri) ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just "Unsupported URI"
---             }
---         Canonical.ReservedWord span word ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column }
---             , severity = CodeMirror.Error
---             , message = Just (word ++ " can't be redefined")
---             }
--- canonicalWarningToDiagnostic : Canonical.Warning -> CodeMirror.Diagnostic
--- canonicalWarningToDiagnostic warning =
---     case warning of
---         Canonical.NoDefinitions ->
---             { from = CodeMirror.LineCol { line = 1, col = 1 }
---             , to = Nothing
---             , severity = CodeMirror.Warning
---             , message = Just "No definitions"
---             }
---         Canonical.NothingGiven span ->
---             { from = CodeMirror.LineCol { line = span.start.row, col = span.start.column - 1 }
---             , to = Just <| CodeMirror.LineCol { line = span.end.row, col = span.end.column - 1 }
---             , severity = CodeMirror.Warning
---             , message = Just "No definitions"
---             }
--- mossLang : CodeMirror.LanguageDef
--- mossLang =
---     { tokens =
---         [ CodeMirror.tokenRule "#.*" "comment"
---         , CodeMirror.tokenRule "\"[^\"]*\"" "string"
---         , CodeMirror.tokenRule "\\b(if|elif|else|iff|each|size|at)\\b" "keyword"
---         , CodeMirror.tokenRule "\\b\\d+\\b" "number"
---         , CodeMirror.tokenRule "[a-zA-Z_-]\\w*" "variableName"
---         , CodeMirror.tokenRule "\\b(True|False)\\b" "bool"
---         ]
---     , completions =
---         [ CodeMirror.completion "if" "keyword"
---         , CodeMirror.completion "iff" "keyword"
---         , CodeMirror.completion "elif" "keyword"
---         , CodeMirror.completion "else" "keyword"
---         , CodeMirror.completion "each" "keyword"
---         , CodeMirror.completion "size" "keyword"
---         , CodeMirror.completion "at" "keyword"
---         ]
---     }
-
-
-resultMerge : Result a a -> a
-resultMerge result =
-    case result of
-        Ok a ->
-            a
-
-        Err a ->
-            a
